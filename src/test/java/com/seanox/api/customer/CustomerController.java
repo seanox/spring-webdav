8a3e2814486a90bb02b78cc90615562da5e1bf2c
seanox <service@seanox.de>
2021-06-12 20:37:26 +0200

    #0000 apiDAV: Implementation

--- src/test/java/com/seanox/api/customer/CustomerController.java
+++ src/test/java/com/seanox/api/customer/CustomerController.java
@@ -1,42 +1,45 @@
 package com.seanox.api.customer;
 
-import com.seanox.apidav.ApiDavFilter;
+import com.seanox.apidav.ApiDavMapping;
 import org.springframework.stereotype.Component;
 
 import java.io.InputStream;
 import java.io.OutputStream;
+
+import static com.seanox.apidav.ApiDavMapping.Type.GET;
+import static com.seanox.apidav.ApiDavMapping.Type.PUT;
 
 // A managed bean is required.
 // There are various annotations for this: e.g. @Component, @Service, @RestController, ...
 @Component
 public class CustomerController {
 
-    @ApiDavFilter.GET(path="/customer/list.xlsx")
+    @ApiDavMapping(type=GET, path="/customer/list.xlsx")
     public void getList(OutputStream output) {
     }
-    @ApiDavFilter.PUT(path="/customer/list.xlsx")
+    @ApiDavMapping(type=PUT, path="/customer/list.xlsx")
     public void putList(InputStream output) {
     }
 
-    @ApiDavFilter.GET(path="/customer/reports/statistic.xlsx")
+    @ApiDavMapping(type=GET, path="/customer/reports/statistic.xlsx")
     public void statistic(OutputStream output) {
     }
 
-    @ApiDavFilter.GET(path="/customer/reports/turnover.xlsx")
+    @ApiDavMapping(type=GET, path="/customer/reports/turnover.xlsx")
     public void turnover(OutputStream output) {
     }
 
-    @ApiDavFilter.GET(path="/marketing/newsletter.pptx")
+    @ApiDavMapping(type=GET, path="/marketing/newsletter.pptx")
     public void getNewsletter(OutputStream output) {
     }
-    @ApiDavFilter.PUT(path="/marketing/newsletter.pptx")
+    @ApiDavMapping(type=PUT, path="/marketing/newsletter.pptx")
     public void putNewsletter(InputStream output) {
     }
 
-    @ApiDavFilter.GET(path="/marketing/sales.pptx")
+    @ApiDavMapping(type=GET, path="/marketing/sales.pptx")
     public void getSales(OutputStream output) {
     }
-    @ApiDavFilter.PUT(path="/marketing/sales.pptx")
+    @ApiDavMapping(type=PUT, path="/marketing/sales.pptx")
     public void putSales(InputStream output) {
     }
 }
