55260c8df682dd414bffe52546a6ec1f02c9752b
seanox <service@seanox.de>
2021-06-18 22:09:31 +0200

    #0000 Test: Update of test cases

---
+++ src/test/java/com/seanox/apidav/CallbackAdapter.java
@@ -0,0 +1,61 @@
+/**
+ * LIZENZBEDINGUNGEN - Seanox Software Solutions ist ein Open-Source-Projekt,
+ * im Folgenden Seanox Software Solutions oder kurz Seanox genannt.
+ * Diese Software unterliegt der Version 2 der GNU General Public License.
+ *
+ * apiDAV, API-WebDAV mapping for Spring Boot
+ * Copyright (C) 2021 Seanox Software Solutions
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of version 2 of the GNU General Public License as published by the
+ * Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+package com.seanox.apidav;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+
+public class CallbackAdapter {
+
+    public static ApiDavAttribute.AttributeCallback createAttributeCallback(final ApiDavAttribute attributeAnnotation,
+            final Object object, final Method method) {
+        return ApiDavAttribute.AttributeCallback.create(attributeAnnotation, object, method);
+    }
+
+    public static ApiDavInput.InputCallback createInputCallback(final ApiDavInput inputAnnotation,
+            final Object object, final Method method) {
+        return ApiDavInput.InputCallback.create(inputAnnotation, object, method);
+    }
+
+    public static ApiDavMapping.MappingCallback createMappingCallback(final ApiDavMapping mappingAnnotation,
+            final Object object, final Method method) throws AnnotationException {
+        return ApiDavMapping.MappingCallback.create(mappingAnnotation, object, method);
+    }
+
+    public static ApiDavProperty.PropertyCallback createPropertyCallback(final ApiDavProperty propertyAnnotation,
+            final Object object, final Method method) {
+        return ApiDavProperty.PropertyCallback.create(propertyAnnotation, object, method);
+    }
+
+    public static Callback createCallback(final Annotation annotation, final Object object, final Method method)
+            throws AnnotationException {
+        if (annotation.annotationType().equals(ApiDavAttribute.class))
+            return CallbackAdapter.createAttributeCallback((ApiDavAttribute)annotation, object, method);
+        else if (annotation.annotationType().equals(ApiDavInput.class))
+            return CallbackAdapter.createInputCallback((ApiDavInput)annotation, object, method);
+        else if (annotation.annotationType().equals(ApiDavMapping.class))
+            return CallbackAdapter.createMappingCallback((ApiDavMapping)annotation, object, method);
+        else if (annotation.annotationType().equals(ApiDavProperty.class))
+            return CallbackAdapter.createPropertyCallback((ApiDavProperty)annotation, object, method);
+        throw new AdapterException("Unsupported annotation type: " + annotation.annotationType());
+    }
+}
