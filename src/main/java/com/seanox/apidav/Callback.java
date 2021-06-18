0eac8d1ea67531f3b60c6c635abcb4cbebed36bc
seanox <service@seanox.de>
2021-06-18 19:59:23 +0200

    #0000 apiDAV: Implementation

---
+++ src/main/java/com/seanox/apidav/Callback.java
@@ -0,0 +1,42 @@
+package com.seanox.apidav;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Getter(AccessLevel.PACKAGE)
+@AllArgsConstructor(access=AccessLevel.PACKAGE)
+abstract class Callback {
+
+    private static final String DATE_FORMAT = "yyyy-MM-dd h-h:mm:ss";
+
+    private String path;
+    private Type   type;
+    private Object object;
+    private Method method;
+
+    static Date convertDateTime(String datetime)
+            throws ParseException {
+        if (datetime.isBlank())
+            return null;
+        return new SimpleDateFormat(DATE_FORMAT).parse(datetime.trim());
+    }
+
+    static String convertText(String text) {
+        if (text.isBlank())
+            return null;
+        return text.trim();
+    }
+
+    enum Type {
+        Mapping, Input, Attribute, Property
+    }
+}
