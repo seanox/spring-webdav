998fd9c3c0da4a6d95a3a464c41cb305d3d84c8b
seanox <service@seanox.de>
2021-06-22 06:48:02 +0200

    #0000 Filter: Implementation

---
+++ src/main/java/com/seanox/apidav/XmlWriter.java
@@ -0,0 +1,241 @@
+/**
+ * LIZENZBEDINGUNGEN - Seanox Software Solutions ist ein Open-Source-Projekt,
+ * im Folgenden Seanox Software Solutions oder kurz Seanox genannt.
+ * Diese Software unterliegt der Version 2 der GNU General Public License.
+ *
+ * apiDAV, API-WebDAV mapping for Spring Boot
+ * Copyright (C) 2021 Seanox Software Solutions
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of version 2 of the GNU General Public License as published by the
+ * Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+package com.seanox.apidav;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+
+/**
+ * Writer for the output of XML data.<br>
+ * <br>
+ * XmlWriter 1.1.0 20210622<br>
+ * Copyright (C) 2021 Seanox Software Solutions<br>
+ * Alle Rechte vorbehalten.
+ *
+ * @author  Seanox Software Solutions
+ * @version 1.1.0 20210622
+ */
+class XmlWriter implements Closeable {
+
+    /** Output stream */
+    private final OutputStream output;
+
+    /** Encoding to be used */
+    private final Charset encoding;
+
+    /** Enumeration of element types */
+    enum ElementType {
+
+        /** opening element */
+        OPENING("<", ">"),
+
+        /** closing element */
+        CLOSING("</", ">"),
+
+        /** Closing element without content */
+        EMPTY("<", "/>");
+
+        /** Opening character sequence */
+        private final String open;
+
+        /** Closing character sequence */
+        private final String close;
+
+        /**
+         * Constructor, creates the ElementType.
+         * @param open  Opening character sequence
+         * @param close Closing character sequence
+         */
+        private ElementType(String open, String close) {
+            this.open  = open;
+            this.close = close;
+        }
+    }
+
+    /**
+     * Constructor, creates the XML writer with UTF-8 encoding.
+     * @param output Output stream.
+     */
+    XmlWriter(final OutputStream output) {
+        this(output, null);
+    }
+
+    /**
+     * Constructor, creates the XML writer with the specified encoding.
+     * @param output   Output stream.
+     * @param encoding Encoding to be used
+     */
+    XmlWriter(final OutputStream output, final Charset encoding) {
+
+        this.output   = output;
+        this.encoding = Objects.isNull(encoding) ? StandardCharsets.UTF_8 : encoding;
+    }
+
+    /**
+     * Writes an XML parameter into the data stream.
+     * @param  space Namespace (short)
+     * @param  note  Namespace note
+     * @param  name  Element name
+     * @param  value Element value
+     * @throws IOException
+     *     In case of faulty access to the data stream
+     */
+    void writeProperty(final String space, final String note, final String name, final String value)
+            throws IOException {
+
+        this.writeElement(space, note, name, ElementType.OPENING);
+        this.writeText(value);
+        this.writeElement(space, note, name, ElementType.CLOSING);
+    }
+
+    /**
+     * Writes an XML parameter into the data stream.
+     * @param  space Namespace (short)
+     * @param  name  Element name
+     * @param  value Element value
+     * @throws IOException
+     *     In case of faulty access to the data stream
+     */
+    void writeProperty(final String space, final String name, final String value)
+            throws IOException {
+
+        this.writeElement(space, name, ElementType.OPENING);
+        this.writeText(value);
+        this.writeElement(space, name, ElementType.CLOSING);
+    }
+
+    /**
+     * Writes an XML parameter as data segment into the data stream.
+     * @param  space Namespace (short)
+     * @param  name  Element name
+     * @param  value Element value
+     * @throws IOException
+     *     In case of faulty access to the data stream
+     */
+    void writePropertyData(final String space, final String name, final String value)
+            throws IOException {
+
+        this.writeElement(space, name, ElementType.OPENING);
+        this.writeText(String.format("<![CDATA[%s]]>", value));
+        this.writeElement(space, name, ElementType.CLOSING);
+    }
+
+    /**
+     * Writes an XML element into the data stream.
+     * @param  space Namespace (short)
+     * @param  name  Element name
+     * @throws IOException
+     *     In case of faulty access to the data stream
+     */
+    void writeProperty(final String space, final String name)
+            throws IOException {
+        this.writeElement(space, name, ElementType.EMPTY);
+    }
+
+    /**
+     * Writes an XML element into the data stream.
+     * @param  space Namespace (short)
+     * @param  name  Element name
+     * @param  type  Element type
+     * @throws IOException
+     *     In case of faulty access to the data stream
+     */
+    void writeElement(final String space, final String name, final ElementType type)
+            throws IOException {
+        this.writeElement(space, null, name, type);
+    }
+
+    /**
+     * Writes an XML element into the data stream.
+     * @param  space Namespace (short)
+     * @param  note  Namespace note
+     * @param  name  Element name
+     * @param  type  Element type
+     * @throws IOException
+     *     In case of faulty access to the data stream
+     */
+    void writeElement(final String space, final String note, final String name, final ElementType type)
+            throws IOException {
+
+        this.writeText(Objects.nonNull(type) ? type.open : ElementType.EMPTY.open);
+        if (space != null
+                && !space.isBlank()) {
+            this.writeText(String.format("%s:%s", space.trim(), name));
+            if (note != null
+                    && !note.isBlank())
+                this.writeText(String.format(" xmlns:%s=\"%s\"", space.trim(), note.trim()));
+        } else this.writeText(name);
+        this.writeText(Objects.nonNull(type) ? type.close : ElementType.EMPTY.close);
+    }
+
+    /**
+     * Writes the passed string as text into the data stream.
+     * @param  text Text
+     * @throws IOException
+     *     In case of faulty access to the data stream
+     */
+    void writeText(final String text)
+            throws IOException {
+        this.output.write(String.valueOf(text).getBytes(this.encoding));
+    }
+
+    /**
+     * Writes the passed string as data segment into the data stream.
+     * @param  data Content of the data segment
+     * @throws IOException
+     *     In case of faulty access to the data stream
+     */
+    void writeData(final String data)
+            throws IOException {
+        this.writeText(String.format("<![CDATA[%s]]>", data));
+    }
+
+    /**
+     * Writes the XML header into the data stream.
+     * @throws IOException
+     *     In case of faulty access to the data stream
+     */
+    void writeXmlHeader()
+            throws IOException {
+        this.writeText(String.format("<?xml version=\"1.0\" encoding=\"%s\"?>", this.encoding.name()));
+    }
+
+    /**
+     * Writes the data in the output buffer to the data stream.
+     * @throws IOException
+     *     In case of faulty access to the data stream
+     */
+    void flush()
+            throws IOException {
+        this.output.flush();
+    }
+
+    @Override
+    public void close()
+            throws IOException {
+        this.output.close();
+    }
+}
---
+++ src/test/java/com/seanox/apidav/XmlWriterAdapter.java
@@ -0,0 +1,123 @@
+/**
+ * LIZENZBEDINGUNGEN - Seanox Software Solutions ist ein Open-Source-Projekt,
+ * im Folgenden Seanox Software Solutions oder kurz Seanox genannt.
+ * Diese Software unterliegt der Version 2 der GNU General Public License.
+ *
+ * apiDAV, API-WebDAV mapping for Spring Boot
+ * Copyright (C) 2021 Seanox Software Solutions
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of version 2 of the GNU General Public License as published by the
+ * Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+package com.seanox.apidav;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Objects;
+
+/**
+ * Testing private parts and/or components visible only in the package requires
+ * an adapter for access.
+ *
+ * Why are the tests not in com.seanox.apidav?
+ * Spring Test is used for the tests. For this @ComponentScan must scan the
+ * package. For the release version, however, it should be ensured that the
+ * library com.seanox.apidav also works without @ComponentScan and therefore
+ * another package is used for the tests of the package com.seanox.apidav.
+ */
+public class XmlWriterAdapter extends XmlWriter {
+
+    public XmlWriterAdapter(final OutputStream output) {
+        super(output);
+    }
+
+    public XmlWriterAdapter(final OutputStream output, final Charset encoding) {
+        super(output, encoding);
+    }
+
+    public enum ElementType {
+
+        OPENING(XmlWriter.ElementType.OPENING),
+        CLOSING(XmlWriter.ElementType.CLOSING),
+        EMPTY(XmlWriter.ElementType.EMPTY);
+
+        final XmlWriter.ElementType elementType;
+
+        private ElementType(XmlWriter.ElementType elementType) {
+            this.elementType = elementType;
+        }
+    }
+
+    @Override
+    public void writeProperty(final String space, final String note, final String name, final String value)
+            throws IOException {
+        super.writeProperty(space, note, name, value);
+    }
+
+    @Override
+    public void writeProperty(final String space, final String name, final String value)
+            throws IOException {
+        super.writeProperty(space, name, value);
+    }
+
+    @Override
+    public void writePropertyData(final String space, final String name, final String value)
+            throws IOException {
+        super.writePropertyData(space, name, value);
+    }
+
+    @Override
+    public void writeProperty(final String space, final String name)
+            throws IOException {
+        super.writeProperty(space, name);
+    }
+
+    public void writeElement(final String space, final String name, final ElementType type)
+            throws IOException {
+        super.writeElement(space, name, type.elementType);
+    }
+
+    public void writeElement(final String space, final String note, final String name, final ElementType type)
+            throws IOException {
+        super.writeElement(space, note, name, type.elementType);
+    }
+
+    @Override
+    public void writeText(final String text)
+            throws IOException {
+        super.writeText(text);
+    }
+
+    @Override
+    public void writeData(final String data)
+            throws IOException {
+        super.writeData(data);
+    }
+
+    @Override
+    public void writeXmlHeader()
+            throws IOException {
+        super.writeXmlHeader();
+    }
+
+    @Override
+    public void flush()
+            throws IOException {
+        super.flush();
+    }
+}
