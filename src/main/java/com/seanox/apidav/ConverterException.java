e39057e34556483448bdca499962a2d0512c9def
seanox <service@seanox.de>
2021-07-08 07:03:49 +0200

    #0000 Filter: Implementation

---
+++ src/main/java/com/seanox/apidav/ConverterException.java
@@ -0,0 +1,39 @@
+/**
+ * LIZENZBEDINGUNGEN - Seanox Software Solutions ist ein Open-Source-Projekt,
+ * im Folgenden Seanox Software Solutions oder kurz Seanox genannt.
+ * Diese Software unterliegt der Version 2 der GNU General Public License.
+ *
+ * apiDAV, API-WebDAV mapping for Spring Boot
+ * Copyright (C) 2021 Seanox Software Solutions
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of version 2 of the GNU General Public License as published by the
+ * Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+package com.seanox.apidav;
+
+/**
+ * ConverterException
+ *
+ * ConverterException 1.0.0 20210708
+ * Copyright (C) 2021 Seanox Software Solutions
+ * All rights reserved.
+ *
+ * @author  Seanox Software Solutions
+ * @version 1.0.0 20210708
+ */
+class ConverterException extends RuntimeException {
+
+    ConverterException(final Exception cause) {
+        super(cause);
+    }
+}
