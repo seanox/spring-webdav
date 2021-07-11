21ad3ed7dd924c48755752ce2b08ebb4baac4f4a
seanox <service@seanox.de>
2021-07-11 08:09:51 +0200

    #0000 Test: Update of test cases

--- src/test/java/com/seanox/api/extras/MetaTestController.java
+++ src/test/java/com/seanox/api/extras/MetaTestController.java
@@ -24,11 +24,15 @@ package com.seanox.api.extras;
 import com.seanox.apidav.ApiDavMapping;
 import com.seanox.apidav.ApiDavMetaMapping;
 import com.seanox.apidav.MetaData;
+import com.seanox.apidav.MetaInputStream;
 import com.seanox.apidav.MetaOutputStream;
 import com.seanox.apidav.MetaProperties;
+import com.seanox.apidav.Properties;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.net.URI;
+import java.util.Objects;
 
 /**
  * Example for the integration of apiDAV into a RestController.
@@ -37,12 +41,12 @@ import java.io.IOException;
  * There are various annotations for this: e.g. @Component, @Service, @RestController, ...
  * The methods and annotations for apiDAV combine well with @RestController.
  *
- * MetaTestController 1.0.0 20210710
+ * MetaTestController 1.0.0 20210711
  * Copyright (C) 2021 Seanox Software Solutions
  * All rights reserved.
  *
  * @author  Seanox Software Solutions
- * @version 1.0.0 20210710
+ * @version 1.0.0 20210711
  */
 @Component
 public class MetaTestController {
@@ -50,7 +54,7 @@ public class MetaTestController {
     // ContentType is set by meta callback.
     // After that, it must be possible to retrieve the value via different ways.
 
-    private static final String MAPPING_A1 = "/extras/meta/a1.txt";
+    public static final String MAPPING_A1 = "/extras/meta/a1.txt";
     @ApiDavMapping(path=MAPPING_A1)
     void testA1(MetaOutputStream outputStream) throws IOException {
         outputStream.write((MAPPING_A1 + " " + outputStream.getContentType()).getBytes());
@@ -60,7 +64,7 @@ public class MetaTestController {
         meta.setContentType("m1");
     }
 
-    private static final String MAPPING_A2 = "/extras/meta/a2.txt";
+    public static final String MAPPING_A2 = "/extras/meta/a2.txt";
     @ApiDavMapping(path=MAPPING_A2)
     void testA2(MetaData metaData, MetaOutputStream outputStream) throws IOException {
         outputStream.write((MAPPING_A2 + " " + metaData).getBytes());
@@ -70,7 +74,7 @@ public class MetaTestController {
         meta.setContentType("m2");
     }
 
-    private static final String MAPPING_A3 = "/extras/meta/a3.txt";
+    public static final String MAPPING_A3 = "/extras/meta/a3.txt";
     @ApiDavMapping(path=MAPPING_A3)
     void testA3(MetaProperties metaProperties, MetaOutputStream outputStream) throws IOException {
         outputStream.write((MAPPING_A3 + " " + metaProperties.getContentType()).getBytes());
@@ -84,17 +88,17 @@ public class MetaTestController {
     // The stand value is only used for untouched fields.
     // If the value null is used, the propfind must omit this attribute.
 
-    private static final String MAPPING_A4 = "/extras/meta/a4.txt";
+    public static final String MAPPING_A4 = "/extras/meta/a4.txt";
     @ApiDavMapping(path=MAPPING_A4)
-    void testB1(MetaProperties metaProperties, MetaOutputStream outputStream) throws IOException {
+    void testA4(MetaProperties metaProperties, MetaOutputStream outputStream) throws IOException {
         outputStream.write((MAPPING_A4 + " " + outputStream.getContentType()).getBytes());
     }
     @ApiDavMetaMapping(path=MAPPING_A4)
-    void testB1(MetaData meta) {
+    void testA4(MetaData meta) {
         meta.setContentType(null);
     }
 
-    private static final String MAPPING_A5 = "/extras/meta/a5.txt";
+    public static final String MAPPING_A5 = "/extras/meta/a5.txt";
     @ApiDavMapping(path=MAPPING_A5)
     void testA5(MetaProperties metaProperties, MetaOutputStream outputStream) throws IOException {
         outputStream.write((MAPPING_A5 + " " + metaProperties.getContentType()).getBytes());
@@ -102,5 +106,81 @@ public class MetaTestController {
     @ApiDavMetaMapping(path=MAPPING_A5)
     void testA5(MetaData meta) {
         meta.setContentType(null);
+    }
+
+    public static final String MAPPING_A6 = "/extras/meta/a6.txt";
+    @ApiDavMapping(path=MAPPING_A6)
+    void testA6(MetaProperties metaProperties, MetaOutputStream outputStream) throws IOException {
+        outputStream.write((MAPPING_A6 + " " + metaProperties.getContentType()).getBytes());
+    }
+    @ApiDavMetaMapping(path=MAPPING_A6)
+    void testA6(MetaData meta) {
+    }
+
+    // Following placeholders for methods parameters are supported: Properties, URI, MetaData
+    // Order and number are not fixed.
+
+    String testB1Result;
+    public static final String MAPPING_B1 = "/extras/meta/b1.txt";
+    @ApiDavMapping(path=MAPPING_B1)
+    void testB1(MetaProperties metaProperties, MetaOutputStream outputStream) throws IOException {
+        outputStream.write(testB1Result.getBytes());
+    }
+    @ApiDavMetaMapping(path=MAPPING_B1)
+    void testB1(MetaData meta1, MetaData meta2) {
+        testB1Result = String.format("%s %s %s %s",
+                meta1.getUri(), Objects.nonNull(meta1), Objects.nonNull(meta2), meta1 == meta2);
+    }
+
+    String testB2Result;
+    public static final String MAPPING_B2 = "/extras/meta/b2.txt";
+    @ApiDavMapping(path=MAPPING_B2)
+    void testB2(MetaProperties metaProperties, MetaOutputStream outputStream) throws IOException {
+        outputStream.write(testB2Result.getBytes());
+    }
+    @ApiDavMetaMapping(path=MAPPING_B2)
+    void testB2(URI uri, MetaData meta1) {
+        testB2Result = String.format("%s %s",
+                meta1.getUri(), Objects.nonNull(meta1));
+    }
+
+    String testB3Result;
+    public static final String MAPPING_B3 = "/extras/meta/b3.txt";
+    @ApiDavMapping(path=MAPPING_B3)
+    void testB3(MetaProperties metaProperties, MetaOutputStream outputStream) throws IOException {
+        outputStream.write(testB3Result.getBytes());
+    }
+    @ApiDavMetaMapping(path=MAPPING_B3)
+    void testB3(Properties properties, URI uri, MetaData meta1) {
+        testB3Result = String.format("%s %s %s %s",
+                meta1.getUri(), Objects.nonNull(properties), Objects.nonNull(uri), Objects.nonNull(meta1));
+    }
+
+    String testB4Result;
+    public static final String MAPPING_B4 = "/extras/meta/b4.txt";
+    @ApiDavMapping(path=MAPPING_B4)
+    void testB4(MetaProperties metaProperties, MetaOutputStream outputStream) throws IOException {
+        outputStream.write(testB4Result.getBytes());
+    }
+    @ApiDavMetaMapping(path=MAPPING_B4)
+    void testB4(URI uri1, Properties properties1, MetaData meta1, URI uri2, Properties properties2, String string,
+                MetaProperties metaProperties, MetaOutputStream outputStream, MetaInputStream inputStream) {
+        testB4Result = String.format("%s %s %s %s %s %s %s %s %s %s %s",
+                meta1.getUri(),
+                Objects.nonNull(uri1), Objects.nonNull(uri2), uri1 == uri2,
+                Objects.nonNull(properties1), Objects.nonNull(properties2), properties1 == properties2,
+                Objects.nonNull(metaProperties),
+                Objects.isNull(string), Objects.isNull(outputStream), Objects.isNull(inputStream));
+    }
+
+    String testB5Result;
+    public static final String MAPPING_B5 = "/extras/meta/b5.txt";
+    @ApiDavMapping(path=MAPPING_B5)
+    void testB5(MetaProperties metaProperties, MetaOutputStream outputStream) throws IOException {
+        outputStream.write(testB5Result.getBytes());
+    }
+    @ApiDavMetaMapping(path=MAPPING_B5)
+    void testB5(Object... objects) {
+        testB5Result = MAPPING_B5 + " " + Objects.isNull(objects);
     }
 }
