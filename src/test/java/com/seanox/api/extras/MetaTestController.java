ca2395a98648ce16af3904211b49740f4f464316
seanox <service@seanox.de>
2021-07-25 12:20:07 +0200

    #0000 Documentation: Update of content

--- src/test/java/com/seanox/api/extras/MetaTestController.java
+++ src/test/java/com/seanox/api/extras/MetaTestController.java
@@ -3,7 +3,7 @@
  * im Folgenden Seanox Software Solutions oder kurz Seanox genannt.
  * Diese Software unterliegt der Version 2 der GNU General Public License.
  *
- * apiDAV, API-WebDAV mapping for Spring Boot
+ * WebDAV mapping for Spring Boot
  * Copyright (C) 2021 Seanox Software Solutions
  *
  * This program is free software; you can redistribute it and/or modify it under
@@ -37,10 +37,10 @@ import java.text.ParseException;
 import java.util.Objects;
 
 /**
- * Test of the annotation {@link ApiDavMetaMapping} functions.
- *
- * MetaTestController 1.0.0 20210719
- * Copyright (C) 2021 Seanox Software Solutions
+ * Test of the annotation {@link ApiDavMetaMapping}.<br>
+ * <br>
+ * MetaTestController 1.0.0 20210719<br>
+ * Copyright (C) 2021 Seanox Software Solutions<br>
  * All rights reserved.
  *
  * @author  Seanox Software Solutions
