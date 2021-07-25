9e262bb98a2f6a06249cb66b56cd9c04574424aa
seanox <service@seanox.de>
2021-07-25 10:09:59 +0200

    #0000 Documentation: Update of content

--- src/main/java/com/seanox/apidav/Properties.java
+++ src/main/java/com/seanox/apidav/Properties.java
@@ -3,7 +3,7 @@
  * im Folgenden Seanox Software Solutions oder kurz Seanox genannt.
  * Diese Software unterliegt der Version 2 der GNU General Public License.
  *
- * apiDAV, API-WebDAV mapping for Spring Boot
+ * WebDAV mapping for Spring Boot
  * Copyright (C) 2021 Seanox Software Solutions
  *
  * This program is free software; you can redistribute it and/or modify it under
