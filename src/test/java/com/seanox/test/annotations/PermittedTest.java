97e11e819727f58f7e3976f0328ca7edca68e71a
seanox <service@seanox.de>
2021-07-25 10:46:00 +0200

    #0000 Documentation: Update of content

--- src/test/java/com/seanox/test/annotations/PermittedTest.java
+++ src/test/java/com/seanox/test/annotations/PermittedTest.java
@@ -3,7 +3,7 @@
  * im Folgenden Seanox Software Solutions oder kurz Seanox genannt.
  * Diese Software unterliegt der Version 2 der GNU General Public License.
  *
- * apiDAV, API-WebDAV mapping for Spring Boot
+ * WebDAV mapping for Spring Boot
  * Copyright (C) 2021 Seanox Software Solutions
  *
  * This program is free software; you can redistribute it and/or modify it under
@@ -28,9 +28,11 @@ import org.junit.jupiter.api.Test;
 
 /**
  * Test the function of the Permitted attribute.
- *
- * PermittedTest 1.0.0 20210724
- * Copyright (C) 2021 Seanox Software Solutions
+ * {@link com.seanox.apidav.ApiDavMapping} and
+ * {@link com.seanox.apidav.ApiDavInputMapping}.<br>
+ * <br>
+ * PermittedTest 1.0.0 20210724<br>
+ * Copyright (C) 2021 Seanox Software Solutions<br>
  * All rights reserved.
  *
  * @author  Seanox Software Solutions
