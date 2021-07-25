7b387e7c6e16037635cfba752d3a3d53a1da8b54
seanox <service@seanox.de>
2021-07-25 11:51:26 +0200

    #0000 Documentation: Update of content

--- src/test/java/com/seanox/api/Application.java
+++ src/test/java/com/seanox/api/Application.java
@@ -3,7 +3,7 @@
  * im Folgenden Seanox Software Solutions oder kurz Seanox genannt.
  * Diese Software unterliegt der Version 2 der GNU General Public License.
  *
- * apiDAV, API-WebDAV mapping for Spring Boot
+ * WebDAV mapping for Spring Boot
  * Copyright (C) 2021 Seanox Software Solutions
  *
  * This program is free software; you can redistribute it and/or modify it under
@@ -36,26 +36,26 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * Example for the integration of apiDAV in a Spring bases application.
- *
+ * Example for the integration of apiDAV in a Spring bases application.<br>
+ * <br>
  * Normally @ComponentScan does not need to be used because Application.class
  * already sets the package com.seanox.api, but in this example there is still
  * a parallel com.seanox.test package. There are no classical UnitTests. Tests
  * are integrated in the application and are executed at startup. For this
  * purpose, the tests are integrated as @components. Therefore @ComponentScan
- * must be configured.
- *
- * Why are the tests not in com.seanox.apidav?
+ * must be configured.<br>
+ * <br>
+ * Why are the tests not in com.seanox.apidav?<br>
  * Spring Test is used for the tests. For this @ComponentScan must scan the
  * package. For the release version, however, it should be ensured that the
  * library com.seanox.apidav also works without @ComponentScan and therefore
- * another package is used for the tests of the package com.seanox.apidav.
- *
+ * another package is used for the tests of the package com.seanox.apidav.<br>
+ * <br>
  * SpringBootServletInitializer to prepare the application to deploy on
- * external servlet container/runner.
- *
- * Application 1.0.0 20210626
- * Copyright (C) 2021 Seanox Software Solutions
+ * external servlet container/runner.<br>
+ * <br>
+ * Application 1.0.0 20210626<br>
+ * Copyright (C) 2021 Seanox Software Solutions<br>
  * All rights reserved.
  *
  * @author  Seanox Software Solutions
