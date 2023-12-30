0debc18fd5e8afde419730fd4b2ea136b30408bc
seanox <service@seanox.de>
2023-12-30 18:46:22 +0100

    #0000 Test: Update of test cases

--- src/test/java/com/seanox/test/XmlWriterTest.java
+++ src/test/java/com/seanox/test/XmlWriterTest.java
@@ -4,7 +4,7 @@
  * Diese Software unterliegt der Version 2 der Apache License.
  *
  * WebDAV mapping for Spring Boot
- * Copyright (C) 2021 Seanox Software Solutions
+ * Copyright (C) 2023 Seanox Software Solutions
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -31,12 +31,16 @@ import java.io.IOException;
  * Test of the XmlWriter functions.
  *
  * @author  Seanox Software Solutions
- * @version 1.0.0 20210815
+ * @version 1.1.0 20231230
  */
 class XmlWriterTest extends AbstractTest {
 
     @Test
     void test_1() throws IOException {
+
+        // UTF-8 encoding and decoding is not easy to do with the strings and
+        // the JRE. Therefore text 1:1 and then see whether the UTF-8 encoding
+        // is in the byte array.
 
         final ByteArrayOutputStream buffer = new ByteArrayOutputStream();
         try (final XmlWriterAdapter xmlWriter = new XmlWriterAdapter(buffer)) {
@@ -61,9 +65,9 @@ class XmlWriterTest extends AbstractTest {
 
             final String output = buffer.toString().replaceAll("<(?!/)", "\n<").trim();
             final String pattern = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-                    "<![CDATA[writeData1-\u00c3\u00b6]]>\n" +
-                    "<space1:name1>writeText1-\u00c3\u00b6</space1:name1>\n" +
-                    "<space2:name2 xmlns:space2=\"note2\">writeText2-\u00c3\u00b6</space2:name2 xmlns:space2=\"note2\">\n" +
+                    "<![CDATA[writeData1-\u00F6]]>\n" +
+                    "<space1:name1>writeText1-\u00F6</space1:name1>\n" +
+                    "<space2:name2 xmlns:space2=\"note2\">writeText2-\u00F6</space2:name2 xmlns:space2=\"note2\">\n" +
                     "<space3:name3/>\n" +
                     "<space4:name4 xmlns:space4=\"note4\"/>\n" +
                     "<space5:name5/>\n" +
@@ -72,6 +76,17 @@ class XmlWriterTest extends AbstractTest {
                     "<space8:name8>\n" +
                     "<![CDATA[value8]]></space8:name8>";
             Assertions.assertEquals(pattern, output);
+
+            final byte[] outputRaw = buffer.toByteArray();
+            int count = 0;
+            for (int cursor = 0; cursor < outputRaw.length -1; cursor++) {
+                int a = outputRaw[cursor];
+                int b = outputRaw[cursor +1];
+                if (a == -61
+                        && b == -74)
+                    count++;
+            }
+            Assertions.assertEquals(3, count);
         }
     }
 }
